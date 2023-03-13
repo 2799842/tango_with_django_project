d11f1ab94f439081c17c760f9bcedc52f90454f2
2799842 <2799842M@student.gla.ac.uk>
2023-03-13 13:21:03 +0000

    URL rango/about change

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rango import views
+from rango.views import about
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', about, name='about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+#Create your views here
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = HttpResponse("Rango says hey there partner! Visit the <a href='/rango/about/'>about</a> page.")
+    return response
 
+def about(request):
+    response = HttpResponse("This is the about page. Go back to the <a href='/rango/'>index</a> page.")
+    return response
