7fc2e012cc4601d196305651842031670a50bb35
2799842 <2799842M@student.gla.ac.uk>
2023-03-16 13:45:52 +0000

    Chapter 3 tests passed

--- rango/tests.py
+++ rango/tests.py
@@ -1,4 +1,1 @@
 from django.test import TestCase
-
-# Create your tests here.
-# message to test commits
--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    response = HttpResponse("Rango says hey there partner! Visit the <a href='/rango/about/'>about</a> page.")
+    response = HttpResponse("Rango says hey there partner! Visit the <a href='/rango/about/'>About</a> page.")
     return response
 
 def about(request):
-    response = HttpResponse("This is the about page. Go back to the <a href='/rango/'>index</a> page.")
+    response = HttpResponse("Rango says here is the about page. Go back to the <a href='/rango/'>Index</a> page.")
     return response
